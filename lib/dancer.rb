9a3147d5dbbc1e31dc9fd38460bafef12dec0138
Eliza Munroe <era3munroe@gmail.com>
2019-08-20 14:24:58 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,21 @@
-require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+#require_relative './class_methods_module.rb'
+#require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+  
+ # extend MetaDancing
+  
+  extend
+    FancyDance::ClassMethods 
+  include 
+    FancyDance::InstanceMethods
+  end
+  #include Dance
+ 
+  #attr_accessor :name
+ 
+  #def initialize(name)
+    #@name = name
+  #end
+#end
